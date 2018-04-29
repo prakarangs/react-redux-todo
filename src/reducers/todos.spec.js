d781707200609dc42c74820503da8a975a956875
kao <kao@aklamio.com>
2018-04-29 21:53:13 +0200

    Add todos spec

--- src/reducers/todos.spec.js
+++ src/reducers/todos.spec.js
@@ -6,4 +6,69 @@ describe('todos Reducer', () => {
 			todos(undefined, {})
 		).toEqual([])
 	})
+
+	it('should handle ADD_TODO', () => {
+		expect(
+			todos([], {
+				type: 'ADD_TODO',
+				id: 0,
+				text: 'My first todo'
+			})
+		).toEqual([{
+			id: 0,
+			text: 'My first todo',
+			completed: false
+		}])
+
+		expect(
+			todos([{
+				id: 0,
+				text: 'My first todo',
+				completed: false
+			}], {
+				type: 'ADD_TODO',
+				id: 1,
+				text: 'Second to do right here!'
+			})
+		).toEqual([{
+			id: 0,
+			text: 'My first todo',
+			completed: false
+		},
+		{
+			id: 1,
+			text: 'Second to do right here!',
+			completed: false
+		}])
+
+	})
+
+	it('should handle TOGGLE_TODO', () => {
+		expect(
+			todos([
+        {
+          text: 'Run the tests',
+          completed: false,
+          id: 1
+        }, {
+          text: 'Use Redux',
+          completed: false,
+          id: 0
+        }
+      ], {
+        type: 'TOGGLE_TODO',
+        id: 1
+      })
+		).toEqual([
+      {
+        text: 'Run the tests',
+        completed: true,
+        id: 1
+      }, {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      }
+    ])
+	})
 })
