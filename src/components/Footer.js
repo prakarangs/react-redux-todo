ca94c702c98e55a6b4ae21d979897558bb61efd6
kao <kao@aklamio.com>
2018-04-29 17:39:00 +0200

    Add containers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
-import React from "react";
-import { VisibilityFilters } from '../actions'
+import React from "react"
+import FilterLink from '../containers/FilterLink'
+import {VisibilityFilters} from '../actions'
 
 const Footer = () => (
 		<p>
-
+				Show: {' '}
+				<FilterLink filter={VisibilityFilters.SHOW_ALL}>
+						All
+				</FilterLink>
+				{', '}
+				<FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>
+						Active
+				</FilterLink>
+				{', '}
+				<FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>
+						Completed
+				</FilterLink>
 		</p>
 )
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+import {createStore} from "redux";
 import todoApp from "./reducers";
 
 const store = createStore(todoApp);
