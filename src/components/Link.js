f8fc3fe9b38edf39383e9090d3c98356aacc4c9b
kao <kao@aklamio.com>
2018-05-02 17:37:23 +0200

    Add containers and connect state & dispatch to props Update UI to receive Props

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,24 +1,22 @@
-import React from "react";
+import React from 'react'
 import PropTypes from 'prop-types'
 
-const Link = ({active, children, onClick}) => {
-		if (active) {
-				return <span>{children}</span>;
-		}
+const Link = ({ active, children, onClick }) => (
+    <button
+       onClick={onClick}
+       disabled={active}
+       style={{
+           marginLeft: '4px',
+       }}
+    >
+      {children}
+    </button>
+)
 
-		return (
-				<a href="" onClick={e => {
-						e.preventDefault();
-						onClick();
-				}}>
-						{children}
-				</a>
-		)
-}
 Link.propTypes = {
-		active: PropTypes.bool.isRequired,
-		children: PropTypes.node.isRequired,
-		onClick: PropTypes.func.isRequired
+  active: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired
 }
 
-export default Link;
+export default Link
